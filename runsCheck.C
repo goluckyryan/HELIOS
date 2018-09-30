e5c3eab2037cd449d21a7092b64207e89f14e588
Ryan@MacAir <goluckyryan@gmail.com>
2018-09-30 19:31:51 +0200

    imporved runsCheck.C

--- runsCheck.C
+++ runsCheck.C
@@ -1,10 +1,13 @@
 {
-   TString rootFile;
+   TString folderPath ="data/gen_run*.root";
+   const char* treeName="gen_tree";
+   
+   
+   //==============================================
+   //TString rootFile;
    
    TFile * f = NULL;
    TTree * tree = NULL;
-   
-   const char* treeName="gen_tree";
    
    ULong64_t e_t[100];
    TBranch  *b_EnergyTimestamp;
@@ -12,21 +15,47 @@
    double time1, time2, dTime;
    ULong64_t firstTime, lastTime;
    
+   TString cmd;
+   cmd.Form(".!ls -1tr %s > runList.txt", folderPath.Data());
+   gROOT->ProcessLine(cmd);
+   
+   ifstream file;
+   file.open("runList.txt");
+   vector<TString> rootFileName;
+   if( file.is_open() ){
+      string x;
+      while( file >> x){
+         rootFileName.push_back(x);
+      }
+   }else{
+       printf("... fail\n");
+       gROOT->ProcessLine(".!rm -f runList.txt");
+       return;
+   }
+   
+   gROOT->ProcessLine(".!rm -f runList.txt"); 
+   
+   
    FILE * paraOut;
    TString filename;
    filename.Form("run_Summary.dat");
-   paraOut = fopen (filename.Data(), "w+");
+   paraOut = fopen (filename, "w+");
    
-   for( int i = 1; i <= 46 ; i++){
-      rootFile.Form("~/ANALYSIS/RUNs/H060_208Pb/gen_run%02d.root", i);
+   int numFile = rootFileName.size();
+   for( int i = 0; i < numFile ; i++){
       
-      f = new TFile (rootFile, "read"); 
+      //printf("%s \n", rootFileName[i].Data());
+      
+      f = new TFile (rootFileName[i], "read");
+      
+      if( !f->IsOpen()) continue; 
+      printf("%15s is loaded.", rootFileName[i].Data());
+      
       tree = (TTree*)f->Get(treeName);
 
       tree->SetBranchAddress("e_t", e_t, &b_EnergyTimestamp);
       
       int totalEvent = tree->GetEntries();
-      
       
       time1 = 0;
       time2 = 0;
@@ -72,20 +101,24 @@
       //printf("%11f sec, %11f sec, %11f sec \n", time1, time2, dTime);
       
       double size = f->GetSize(); // in byte
-      printf("=====> /// %15s //// is loaded. Total #Entry: %10d, size: %6.1f MB, duration: %10.2f sec = %7.2f min = %7.2f hr\n", 
-               rootFile.Data(),  totalEvent, size/1024/1024, dTime, dTime/60., dTime/60./60.);
+      printf("#Entry: %10d, size: %6.1f MB, duration: %10.2f sec = %7.2f min = %7.2f hr\n", 
+               totalEvent, size/1024/1024, dTime, dTime/60., dTime/60./60.);
       
       
       if( i == 1 ) {
-         fprintf(paraOut, "%50s, %10s, %8s, %11s\n", "file",  "#event", "size[MB]", "duration[s]");   
+         fprintf(paraOut, "%50s, %10s, %8s, %11s, %11s, %11s\n", "file",  "#event", "size[MB]", "duration[s]", "[min]", "[hour]");   
       
       }   
-      fprintf(paraOut, "%50s, %10d, %8.2f, %11.3f\n",rootFile.Data(),  totalEvent, size/1024/1024, dTime);   
+      fprintf(paraOut, "%50s, %10d, %8.2f, %11.3f, %11.3f, %11.3f\n", rootFileName[i].Data(),  totalEvent, size/1024/1024, dTime, dTime/60., dTime/60./60.);   
       fflush(paraOut);   
+      
+      
    }
       
    fclose(paraOut);
    
    printf("=========== saved summery to %s.\n", filename.Data());
+   
+   gROOT->ProcessLine(".q");
    
 }
